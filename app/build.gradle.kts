33312c5cae11eb358910bfe8de7b59ee8064090d
MajoritySky2496 <gzag2496@gmail.com>
2023-12-06 16:41:52 +0500

    Подключил app метрику

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,4 +71,7 @@ dependencies {
     //Gif
     implementation ("pl.droidsonroids.gif:android-gif-drawable:1.2.22")
 
+    // AppMetrica SDK
+    implementation ("com.yandex.android:mobmetricalib:5.3.0")
+
 }
--- app/src/main/java/com/example/bankiapplication/util/app/App.kt
+++ app/src/main/java/com/example/bankiapplication/util/app/App.kt
@@ -6,16 +6,27 @@ import com.example.bankiapplication.di.interactorModule
 import com.example.bankiapplication.di.permissionModule
 import com.example.bankiapplication.di.repositoryModule
 import com.example.bankiapplication.di.viewModelModule
+import com.yandex.metrica.YandexMetrica
+import com.yandex.metrica.YandexMetricaConfig
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
+
 
 class App:Application() {
     override fun onCreate() {
         super.onCreate()
+
+        val config = YandexMetricaConfig.newConfigBuilder(APP_METRICA_KEY).build()
+        YandexMetrica.activate(applicationContext, config)
+        YandexMetrica.enableActivityAutoTracking(this)
         startKoin {
             androidContext(this@App)
             modules(dataModule, interactorModule, repositoryModule, viewModelModule, permissionModule)
         }
 
+
+    }
+    companion object{
+        const val APP_METRICA_KEY = "e5f43a44-fe39-428e-bbab-47903228fad5"
     }
 }
